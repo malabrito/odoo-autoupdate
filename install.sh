5b4c783a19929e0cc42a4099b3c3c6dce4f6050b
malabrito <mbrito@waycarbon.com>
2016-11-26 10:45:29 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,20 +12,30 @@ sudo apt-get update
 sudo apt-get upgrade
 
 
-echo "Step 2 - Configure a Linux user for Odoo"
+echo "Step 1 - Configure a Linux user for Odoo"
 #Create a new user called odoo with home directory /opt/odoo and the group odoo. You can do it with this command:
 sudo adduser --system --home=/opt/odoo --group odoo
 #and create a new directory for Odoo in the /var/lib/ directory.
 mkdir -p /var/lib/odoo
 
-echo "Step 3 - Install and Configure PostgreSQL"
+echo "Step 2 - Install and Configure PostgreSQL"
 #Install postgreSQL with this apt-get command:
 sudo apt-get install postgresql -y
+su - postgres
+createuser --createdb --username postgres --no-createrole --no-superuser --pwprompt odoo
+Swing2016
 
-echo "Step 4 - Install Python dependencies for Odoo 10"
-sudo apt-get install python-dateutil python-docutils python-feedparser python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi poppler-utils python-pip python-pypdf python-passlib python-decorator gcc python-dev mc bzr python-setuptools python-markupsafe python-reportlab-accel python-zsi python-yaml python-argparse python-openssl python-egenix-mxdatetime python-usb python-serial lptools make python-pydot python-psutil python-paramiko poppler-utils python-pdftools antiword python-requests python-xlsxwriter python-suds python-psycogreen python-ofxparse python-gevent
 
-echo "Step 5 - Install Odoo Web Dependencies"
+echo "Step 3 - Install Python dependencies for Odoo 10"
+sudo apt-get install python-dev libxml2-dev libxslt1-dev zlib1g-dev -y
+sudo apt-get install python-cups python-dateutil python-decorator python-docutils python-feedparser \
+python-gdata python-geoip python-gevent python-imaging python-jinja2 python-ldap python-libxslt1 \
+python-lxml python-mako python-mock python-openid python-passlib python-psutil python-psycopg2 \
+python-pybabel python-pychart python-pydot python-pyparsing python-pypdf python-reportlab python-requests \
+python-simplejson python-tz python-unicodecsv python-unittest2 python-vatnumber python-vobject \
+python-werkzeug python-xlwt python-yaml wkhtmltopdf -y
+
+echo "Step 4 - Install Odoo Web Dependencies"
 apt-get install -y npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g less less-plugin-clean-css
